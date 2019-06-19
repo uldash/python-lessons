b53e07d10ff5a51c730634de7918a55fd9690145
Yuri <uldashevue@gmail.com>
2019-06-19 22:12:36 +0300

    rename to Different and add Different/stackoverflow

---
+++ Different/stackoverflow/stackoverflow.py
@@ -0,0 +1,25 @@
+
+
+def download_survey():
+    import os
+    import shutil
+    import zipfile
+    import requests
+
+    request = requests.get(
+        "https://drive.google.com/uc?export=download&id=1_9On2-nsBQIw3JiY43sWbrF8EjrqrR4U")
+    with open("survey2018.zip", "wb") as file:
+        file.write(request.content)
+
+    with zipfile.ZipFile('survey2018.zip') as zip:
+        zip.extractall('survey2018')
+
+    shutil.move('survey2018/survey_results_public.csv', 'mysurvey.csv')
+    shutil.rmtree('survey2018')
+    os.remove('survey2018.zip')
+
+
+download_survey()
+
+
+# TODO 2019.06.19: refactor this code!
