eda9bc18b02007a1ac1e9eaec32b85c722b66ce1
Yuri <uldashevue@gmail.com>
2019-06-24 19:42:31 +0300

    yet another commit

---
+++ MarkLutc/Программирование на Python/03 Как пользователь запускает программы/script1/script1.py
@@ -0,0 +1,8 @@
+import sys  # load lib module
+
+print(sys.platform)
+print(2 ** 100)
+x = "SPAM'"
+print(x * 8)
+
+input("PRESS ENTER TO EXIT")
---
+++ MarkLutc/Программирование на Python/03 Как пользователь запускает программы/threenames/threenames.py
@@ -0,0 +1,2 @@
+a, b, c = "dead", "parrot", "sketch"
+print(a, b, c)
